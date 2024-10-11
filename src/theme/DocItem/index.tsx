8742e831740ef1e39a152401e419f5821f6d9703
SiegeSailor <siegesailor@gmail.com>
2024-10-11 15:42:44 -0400

    feature: Swizzle BlogPostPage and DocItem

--- src/theme/DocItem/index.tsx
+++ src/theme/DocItem/index.tsx
@@ -1,17 +1,21 @@
-import React from "react";
-import DocItem from "@theme-original/DocItem";
-import type DocItemType from "@theme/DocItem";
-import type { WrapperProps } from "@docusaurus/types";
+import React from 'react';
+import {HtmlClassNameProvider} from '@docusaurus/theme-common';
+import {DocProvider} from '@docusaurus/plugin-content-docs/client';
+import DocItemMetadata from '@theme/DocItem/Metadata';
+import DocItemLayout from '@theme/DocItem/Layout';
+import type {Props} from '@theme/DocItem';
 
-import Comment from "@site/src/components/Comment";
-
-type Props = WrapperProps<typeof DocItemType>;
-
-export default function DocItemWrapper(props: Props): JSX.Element {
+export default function DocItem(props: Props): JSX.Element {
+  const docHtmlClassName = `docs-doc-id-${props.content.metadata.id}`;
+  const MDXComponent = props.content;
   return (
-    <>
-      <DocItem {...props} />
-      <Comment />
-    </>
+    <DocProvider content={props.content}>
+      <HtmlClassNameProvider className={docHtmlClassName}>
+        <DocItemMetadata />
+        <DocItemLayout>
+          <MDXComponent />
+        </DocItemLayout>
+      </HtmlClassNameProvider>
+    </DocProvider>
   );
 }
