7e8d8a9f7084d4a93f9996b26c1d103668ac3be1
SiegeSailor <siegesailor@gmail.com>
2025-04-22 01:53:52 -0400

    feature: Enhance Menu and Search components with keyboard shortcuts and improved styling

--- client/src/component/Header/Menu.tsx
+++ client/src/component/Header/Menu.tsx
@@ -1,44 +1,60 @@
 "use client";
 
 import React from "react";
+import { Kbd } from "@heroui/kbd";
 import { link } from "@heroui/theme";
 import { NavbarMenu, NavbarMenuItem } from "@heroui/navbar";
 import { usePathname } from "next/navigation";
 import * as next from "next";
 import clsx from "clsx";
 import NextLink from "next/link";
+import { Button } from "@heroui/button";
 
 import Search from "@/component/Search";
 
-const pages: { label: string; href: next.Route }[] = [
-  { label: "Home", href: "/" },
-  { label: "Blog", href: "/blog" },
-  { label: "Note", href: "/note" },
-  { label: "Profile", href: "/profile" },
-  { label: "Project", href: "/project" },
-  { label: "Contact", href: "/contact" },
+const pages: { label: string; href: next.Route; press: string }[] = [
+  { label: "Home", href: "/", press: "H" },
+  { label: "Blog", href: "/blog", press: "B" },
+  { label: "Note", href: "/note", press: "N" },
+  { label: "Profile", href: "/profile", press: "P" },
+  { label: "Project", href: "/project", press: "R" },
+  { label: "Contact", href: "/contact", press: "C" },
 ];
 
 export default function () {
   const pathname = usePathname();
   return (
     <NavbarMenu>
-      <Search />
-      <div className="mx-4 mt-2 flex flex-col gap-2">
-        {pages.map((item) => (
-          <NavbarMenuItem key={item.label} isActive={pathname === item.href}>
-            <NextLink
-              className={clsx(
-                link({ color: "foreground" }),
-                "data-[active=true]:text-primary data-[active=true]:font-medium"
-              )}
-              color="foreground"
-              href={item.href}
-            >
-              {item.label}
-            </NextLink>
-          </NavbarMenuItem>
-        ))}
+      <div className="sm:hidden">
+        <Search />
+      </div>
+      <div className="mt-2 flex flex-col gap-2 text-center">
+        {pages.map((item) => {
+          const isActive = pathname === item.href;
+          return (
+            <NavbarMenuItem key={item.label} isActive={isActive}>
+              <NextLink href={item.href}>
+                <Button
+                  endContent={<Kbd keys={["command"]}>{item.press}</Kbd>}
+                  size="md"
+                  variant={isActive ? "flat" : "light"}
+                  className="w-full md:w-6/12 lg:w-8/12"
+                  color={isActive ? "primary" : "default"}
+                >
+                  <span
+                    data-active={isActive}
+                    className={clsx(
+                      link({ color: "foreground" }),
+                      "data-[active=true]:text-primary data-[active=true]:font-medium"
+                    )}
+                  >
+                    {item.label}
+                  </span>
+                </Button>
+              </NextLink>
+            </NavbarMenuItem>
+          );
+        })}
       </div>
     </NavbarMenu>
   );
--- client/src/component/Search.tsx
+++ client/src/component/Search.tsx
@@ -1,41 +1,26 @@
 import React from "react";
 import { RiSearch2Line } from "react-icons/ri";
-import { Button } from "@heroui/button";
 import { Kbd } from "@heroui/kbd";
+import { Input } from "@heroui/input";
 
 export default function () {
-  //   return <Input
-  //   aria-label="Search"
-  //   classNames={{
-  //     inputWrapper: "bg-default-100",
-  //     input: "text-sm",
-  //   }}
-  //   endContent={
-  //     <Kbd className="hidden lg:inline-block" keys={["command"]}>
-  //       K
-  //     </Kbd>
-  //   }
-  //   labelPlacement="outside"
-  //   placeholder="Search..."
-  //   startContent={
-  //     <SearchIcon className="text-base text-default-400 pointer-events-none flex-shrink-0" />
-  //   }
-  //   type="search"
-  // />
   return (
-    <div>
-      <Button
-        startContent={
-          <RiSearch2Line
-            size="1.25rem"
-            className="text-base text-default-400 pointer-events-none flex-shrink-0"
-          />
-        }
-        size="md"
-        variant="bordered"
-      >
-        Type <Kbd keys={["command"]}>S</Kbd> to search
-      </Button>
-    </div>
+    <Input
+      aria-label="Search"
+      classNames={{
+        inputWrapper: ["bg-default-100", "sm:w-52"],
+        input: ["text-sm"],
+      }}
+      endContent={<Kbd keys={["command"]}>S</Kbd>}
+      labelPlacement="outside"
+      placeholder="Search"
+      startContent={
+        <RiSearch2Line
+          size="1.25rem"
+          className="text-base text-default-400 pointer-events-none flex-shrink-0"
+        />
+      }
+      type="search"
+    />
   );
 }
