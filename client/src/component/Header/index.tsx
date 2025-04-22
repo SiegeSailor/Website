7a8e3bc3edc3254e6cbf4248a54211c553117bf9
SiegeSailor <siegesailor@gmail.com>
2025-04-22 02:03:07 -0400

    refactor: Update Header and Search components for improved structure and styling

--- client/src/component/Header/index.tsx
+++ client/src/component/Header/index.tsx
@@ -5,8 +5,10 @@ import {
   NavbarContent,
   NavbarMenuToggle,
   NavbarBrand,
+  NavbarItem,
 } from "@heroui/navbar";
 import NextLink from "next/link";
+import { Divider } from "@heroui/divider";
 
 import ThemeSwitch from "@/component/ThemeSwitch";
 import Search from "@/component/Search";
@@ -16,23 +18,22 @@ import { concatTitle } from "@/helper";
 export default function () {
   return (
     <Navbar maxWidth="md" position="sticky">
-      <NavbarContent
-        className="hidden sm:flex basis-1/5 sm:basis-full"
-        justify="start"
-      >
-        <Search />
-      </NavbarContent>
-      <NavbarContent className="basis-1/5 sm:basis-full" justify="center">
+      <NavbarContent className="basis-1/5 sm:basis-full" justify="start">
         <NavbarBrand as="li" className="gap-3 max-w-fit">
           <NextLink className="flex justify-center items-center gap-1" href="/">
             <h1 className="font-bold text-inherit">{concatTitle()}</h1>
           </NextLink>
         </NavbarBrand>
       </NavbarContent>
+
       <NavbarContent
         className="basis-1/5 sm:basis-full align-middle"
         justify="end"
       >
+        <NavbarItem className="hidden sm:flex">
+          <Search />
+        </NavbarItem>
+        <Divider orientation="vertical" className="hidden sm:flex h-6" />
         <RiAliensLine className="text-default-500" size="1.25rem" />
         <ThemeSwitch />
         <NavbarMenuToggle />
--- client/src/component/Search.tsx
+++ client/src/component/Search.tsx
@@ -1,26 +1,24 @@
 import React from "react";
 import { RiSearch2Line } from "react-icons/ri";
 import { Kbd } from "@heroui/kbd";
-import { Input } from "@heroui/input";
+import { Button } from "@heroui/button";
 
 export default function () {
   return (
-    <Input
+    <Button
       aria-label="Search"
-      classNames={{
-        inputWrapper: ["bg-default-100", "sm:w-52"],
-        input: ["text-sm"],
-      }}
+      className="w-full sm:w-48"
+      radius="full"
       endContent={<Kbd keys={["command"]}>S</Kbd>}
-      labelPlacement="outside"
-      placeholder="Search"
+      variant="bordered"
       startContent={
         <RiSearch2Line
           size="1.25rem"
           className="text-base text-default-400 pointer-events-none flex-shrink-0"
         />
       }
-      type="search"
-    />
+    >
+      Search
+    </Button>
   );
 }
