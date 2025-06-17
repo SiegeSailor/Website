6a2e8988dcb86f48efa8daf2399b7dcde423f2e7
SiegeSailor <siegesailor@gmail.com>
2025-06-16 21:17:40 -0400

    refactor: Update ButtonCopy component to accept content prop; simplify clipboard functionality and improve CodeBlock integration

--- client/component/ButtonCopy.tsx
+++ client/component/ButtonCopy.tsx
@@ -1,39 +1,38 @@
 "use client";
 
-import { forwardRef, useState } from "react";
+import { ComponentProps, useState } from "react";
 import { CopyIcon, CheckIcon } from "lucide-react";
 import { Button } from "@heroui/react";
 import clsx from "clsx";
 
-export default forwardRef<HTMLPreElement, Readonly<{ title?: string }>>(
-  ({ title = "Copy Code" }, ref) => {
-    const [isCopied, setIsCopied] = useState(false);
+export default function ({
+  content,
+  ...props
+}: ComponentProps<typeof Button> & Readonly<{ content: string }>) {
+  const [isCopied, setIsCopied] = useState(false);
 
-    return (
-      <Button
-        className={clsx(
-          "absolute top-2 right-2",
-          "text-default-500 hover:text-default-700",
-          "dark:text-default-400 dark:hover:text-default-200"
-        )}
-        onPress={() => {
-          if (ref && "current" in ref && ref.current) {
-            navigator.clipboard.writeText(ref.current.innerText);
+  return (
+    <Button
+      className={clsx(
+        "absolute top-2 right-2",
+        "text-default-500 hover:text-default-700",
+        "dark:text-default-400 dark:hover:text-default-200"
+      )}
+      onPress={() => {
+        navigator.clipboard.writeText(content);
 
-            if (!isCopied) {
-              setIsCopied(true);
-              setTimeout(() => setIsCopied(false), 2000);
-            }
-          }
-        }}
-        title={title}
-        isIconOnly
-        size="sm"
-        variant="light"
-        radius="full"
-      >
-        {isCopied ? <CheckIcon size="1.25rem" /> : <CopyIcon size="1.25rem" />}
-      </Button>
-    );
-  }
-);
+        if (!isCopied) {
+          setIsCopied(true);
+          setTimeout(() => setIsCopied(false), 2000);
+        }
+      }}
+      isIconOnly
+      size="sm"
+      variant="light"
+      radius="full"
+      {...props}
+    >
+      {isCopied ? <CheckIcon size="1.25rem" /> : <CopyIcon size="1.25rem" />}
+    </Button>
+  );
+}
--- client/component/CodeBlock.tsx
+++ client/component/CodeBlock.tsx
@@ -7,20 +7,57 @@ import highlight from "highlight.js";
 
 import ButtonCopy from "@/component/ButtonCopy";
 
+function parse(content: string) {
+  const lines = content.split("\n");
+
+  const result: { lines: string[]; isHighlighted: boolean }[] = [];
+  let buffer: string[] = [];
+  let isInHighlight = false;
+
+  for (const line of lines) {
+    if (line.includes("highlight-start")) {
+      if (buffer.length > 0) {
+        result.push({ lines: buffer, isHighlighted: false });
+        buffer = [];
+      }
+      isInHighlight = true;
+      continue;
+    }
+
+    if (line.includes("highlight-end")) {
+      if (buffer.length > 0) {
+        result.push({ lines: buffer, isHighlighted: true });
+        buffer = [];
+      }
+      isInHighlight = false;
+      continue;
+    }
+    buffer.push(line);
+  }
+
+  if (buffer.length > 0)
+    result.push({ lines: buffer, isHighlighted: isInHighlight });
+
+  return result;
+}
+
 export default function ({
   children,
   className,
 }: Readonly<{
-  children: ReactNode;
+  children: string;
   className?: string;
 }>) {
-  const refPre = useRef<HTMLPreElement>(null);
+  const refItems = useRef<(HTMLDivElement | null)[]>(null);
 
   const [isHighlighted, setIsHighlighted] = useState(false);
 
   useEffect(() => {
-    if (refPre.current) {
-      highlight.highlightElement(refPre.current);
+    if (refItems.current) {
+      for (const item of refItems.current) {
+        if (item === null) continue;
+        highlight.highlightElement(item);
+      }
       setIsHighlighted(true);
     }
   }, []);
@@ -31,17 +68,35 @@ export default function ({
       isLoaded={isHighlighted}
     >
       <pre
-        ref={refPre}
         className={clsx(
-          "py-4 px-6",
+          "py-4",
           "rounded-medium",
           "overflow-x-auto",
-          "text-small"
+          "text-small",
+          "hljs language-stylus"
         )}
       >
-        {children}
+        {parse(children).map((value, index) => {
+          return (
+            <div
+              ref={(element) => {
+                if (refItems.current === null) {
+                  refItems.current = [];
+                }
+                refItems.current[index] = element;
+              }}
+              key={index}
+              className={clsx(
+                value.isHighlighted ? "bg-default-700" : "bg-transparent",
+                "whitespace-pre px-6"
+              )}
+            >
+              {value.lines.join("\n")}
+            </div>
+          );
+        })}
       </pre>
-      <ButtonCopy ref={refPre} />
+      <ButtonCopy content={children} isDisabled={!isHighlighted} />
     </Skeleton>
   );
 }
