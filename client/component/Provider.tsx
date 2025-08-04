d57366d35b82ba06099a854dcaf934f6e943d9f1
JinYu.Zhang <39463416+SiegeSailor@users.noreply.github.com>
2025-08-04 03:17:42 -0400

    refactor: Remove unused context and simplify Provider component structure

--- client/component/Provider.tsx
+++ client/component/Provider.tsx
@@ -1,23 +1,11 @@
 "use client";
 
-import { ReactNode, ComponentProps, createContext, useContext } from "react";
+import { ReactNode, ComponentProps } from "react";
 import { HeroUIProvider } from "@heroui/react";
 import { ThemeProvider } from "next-themes";
 import { useRouter } from "next/navigation";
 
-const defaultRootContextValues = {} as const;
-
-const RootContext = createContext(defaultRootContextValues);
-
-export function useRootContext() {
-  return useContext(RootContext);
-}
-
-export default function ({
-  children,
-}: Readonly<{
-  children: ReactNode;
-}>) {
+export default function ({ children }: Readonly<{ children: ReactNode }>) {
   const push = useRouter().push as ComponentProps<
     typeof HeroUIProvider
   >["navigate"];
@@ -25,9 +13,7 @@ export default function ({
   return (
     <HeroUIProvider navigate={push}>
       <ThemeProvider attribute="class" defaultTheme="light" enableSystem>
-        <RootContext.Provider value={defaultRootContextValues}>
-          {children}
-        </RootContext.Provider>
+        {children}
       </ThemeProvider>
     </HeroUIProvider>
   );
