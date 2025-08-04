81ec2730549de35b25af688c5c9349bc43963498
JinYu.Zhang <39463416+SiegeSailor@users.noreply.github.com>
2025-08-04 03:08:10 -0400

    refactor: Enhance Link component by adding routing logic and improving external link handling

--- client/component/Link.tsx
+++ client/component/Link.tsx
@@ -1,14 +1,23 @@
-import { ComponentProps } from "react";
+"use client";
+
+import { ComponentProps, useEffect } from "react";
 import { Link } from "@heroui/react";
+import { Route } from "next";
+import { useRouter } from "next/navigation";
 import clsx from "clsx";
 
 export default function ({ ...props }: ComponentProps<typeof Link>) {
   const isExternal = props.href?.startsWith("http");
 
+  const router = useRouter();
+
+  useEffect(() => {
+    if (!isExternal) router.prefetch(props.href as Route);
+  }, [isExternal, props.href, router]);
+
   return (
     <Link
       color="foreground"
-      download
       isExternal={isExternal}
       showAnchorIcon={isExternal}
       target={isExternal ? "_blank" : "_self"}
