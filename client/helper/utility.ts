4dc47a957a3998a1cde5fed1be7a21e9b024b85c
JinYu.Zhang <JinYu.Zhang@coopersurgical.com>
2025-09-08 03:25:13 -0400

    feature: Add getHexByHSLValue function to convert HSL to HEX color format

--- client/helper/utility.ts
+++ client/helper/utility.ts
@@ -35,3 +35,26 @@ export function getValues<T extends object>(entity: T) {
 export function getKeys<T extends object>(entity: T) {
   return Object.keys(entity) as (keyof T)[];
 }
+
+export function getHexByHSLValue(input = "0 0% 0%") {
+  const [hue, saturation, lightness] = input
+    .split(" ")
+    .map((number) => parseInt(number, 10));
+
+  const saturationDecimal = saturation / 100;
+  const lightnessDecimal = lightness / 100;
+
+  const calculateColor = (channelOffset: number) => {
+    const huePosition = (channelOffset + hue / 30) % 12;
+    const chroma =
+      saturationDecimal * Math.min(lightnessDecimal, 1 - lightnessDecimal);
+    const colorValue =
+      lightnessDecimal -
+      chroma * Math.max(Math.min(huePosition - 3, 9 - huePosition, 1), -1);
+    return Math.round(255 * colorValue)
+      .toString(16)
+      .padStart(2, "0");
+  };
+
+  return `#${calculateColor(0)}${calculateColor(8)}${calculateColor(4)}`;
+}
