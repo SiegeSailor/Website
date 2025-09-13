57bc63f27e02a722bda743f52108fa12ecc4933a
JinYu.Zhang <JinYu.Zhang@coopersurgical.com>
2025-09-13 01:46:10 -0400

    refactor: Remove Go from SKILL array

--- client/setting/home.ts
+++ client/setting/home.ts
@@ -58,7 +58,7 @@ export const EXPERIENCE: Readonly<{ title: string; time: string }[]> = [
 ] as const;
 
 export const SKILL: (keyof typeof TECHNOLOGY_ICON)[][] = [
-  ["Python", "TypeScript", "JavaScript", "Bash", "C++", ".NET", "Go"],
+  ["Python", "TypeScript", "JavaScript", "Bash", "C++", ".NET"],
   ["Docker", "AWS", "GCP", "GitHub", "GitLab", "Linux", "Kubernetes"],
   ["MongoDB", "MySQL", "Redis", "RabbitMQ", "Nginx", "Terraform"],
   ["Django", "Flask", "Express.js", "Next.js", "React.js"],
