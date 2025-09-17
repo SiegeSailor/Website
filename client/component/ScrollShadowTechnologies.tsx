c7c827cedf675155436b8ed0fc87a1e3e83009a7
JinYu.Zhang <JinYu.Zhang@coopersurgical.com>
2025-09-17 01:12:45 -0400

    refactor: Simplify technology rendering logic in ScrollShadowTechnologies component

--- client/component/ScrollShadowTechnologies.tsx
+++ client/component/ScrollShadowTechnologies.tsx
@@ -1,11 +1,21 @@
 "use client";
 
-import { ComponentProps } from "react";
+import { ComponentProps, ReactNode } from "react";
 
 import { getArticleByFilename } from "@/helper/server/article";
 import IconTechnology from "@/component/IconTechnology";
 import Link from "@/component/Link";
 import ScrollShadowChips from "@/component/ScrollShadowChips";
+
+function renderItem(item: ReactNode, isLink: boolean, technology: string) {
+  return isLink ? (
+    <Link href={`/blog?technologies=${technology}`} isPlain>
+      {item}
+    </Link>
+  ) : (
+    item
+  );
+}
 
 export default function ({
   isLink = true,
@@ -24,22 +34,16 @@ export default function ({
     <ScrollShadowChips
       {...props}
       row={technologies.map((technology) => ({
-        startContent: (
-          <Link href={`/blog?technologies=${technology}`} isPlain>
-            <IconTechnology
-              key={technology}
-              {...propsIcon}
-              technology={technology}
-            />
-          </Link>
-        ),
-        children: isLink ? (
-          <Link href={`/blog?technologies=${technology}`} isPlain>
-            {technology}
-          </Link>
-        ) : (
+        startContent: renderItem(
+          <IconTechnology
+            key={technology}
+            {...propsIcon}
+            technology={technology}
+          />,
+          isLink,
           technology
         ),
+        children: renderItem(technology, isLink, technology),
       }))}
     />
   );
