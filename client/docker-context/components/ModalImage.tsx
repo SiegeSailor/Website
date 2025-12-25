438f3e3601f59a3d668c263f4b89ad09245743a2
JinYu.Zhang <JinYu.Zhang@coopersurgical.com>
2025-12-25 18:00:58 -0500

    style(client): remove h-max from ModalImage for responsive displays

--- client/docker-context/components/ModalImage.tsx
+++ client/docker-context/components/ModalImage.tsx
@@ -78,7 +78,7 @@ export default function ({
         {...propsImageThumbnail}
         onClick={handleOpen}
         className={clsx(
-          "cursor-pointer w-max h-max",
+          "cursor-pointer w-max",
           { "mx-auto": !isShieldsBadge },
           propsImageThumbnail?.className
         )}
