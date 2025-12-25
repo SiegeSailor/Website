df5b47869f7f96764f1956d44db79327004e39c7
JinYu.Zhang <JinYu.Zhang@coopersurgical.com>
2025-12-25 00:15:39 -0500

    style(client): allow not-found and error component center

--- client/docker-context/app/(child)/layout.tsx
+++ client/docker-context/app/(child)/layout.tsx
@@ -6,7 +6,7 @@ export default async function ({
   children,
 }: Readonly<{ children: ReactNode }>) {
   return (
-    <div className="w-full h-full">
+    <div className="w-full h-full max-h-[calc(100%-4rem)]">
       <div className="max-w-content mx-auto p-4">
         <Breadcrumb />
       </div>
