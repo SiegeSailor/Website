56bf73dcb4135fa555a7edcaa3adeec269e51c7c
JinYu.Zhang <JinYu.Zhang@coopersurgical.com>
2025-12-29 01:30:34 -0500

    fix(client): add comments to clarify loading behavior in blog and profile pages

--- client/docker-context/app/(child)/blog/[date]/loading.tsx
+++ client/docker-context/app/(child)/blog/[date]/loading.tsx
@@ -1,5 +1,6 @@
 import SpinnerCenter from "@/components/SpinnerCenter";
 
+// Sub-level `loading.tsx` doesn't work with dynamic route segments.
 export default async function () {
   return <SpinnerCenter />;
 }
--- client/docker-context/app/(child)/profile/loading.tsx
+++ client/docker-context/app/(child)/profile/loading.tsx
@@ -1,5 +1,6 @@
 import SpinnerCenter from "@/components/SpinnerCenter";
 
+// Breadcrumb in `../layout.tsx` doesn't show up immediately.
 export default async function () {
   return <SpinnerCenter />;
 }
